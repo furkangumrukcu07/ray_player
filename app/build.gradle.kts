bfc489071a0a00c888ce07d9c39edf5a8fa83dfa
macbook <furkan.gumrukcu@idea.com.tr>
2026-08-26 16:12:12 +0300

    Bump version to v1.3.5 (versionCode 9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.ray.iptv.player"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.3.4"
+        versionCode = 9
+        versionName = "1.3.5"
         vectorDrawables.useSupportLibrary = true
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a")
